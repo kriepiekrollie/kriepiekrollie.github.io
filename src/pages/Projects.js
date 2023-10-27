084409c2c819ed53fa0ab6e69dd9342ec1106394
kriepiekrollie <benjamin.kleyn@gmail.com>
2023-10-27 13:54:42 +0200

    Fixed links to external sites.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Projects.css";
 
 export default function Projects() {
@@ -9,7 +10,7 @@ export default function Projects() {
         <div className="projectItem">
           <img src="polyhedra.jpg" />
           <div className="projectText">
-            <a href="kriepiekrollie.github.io/polyhedra"><h2> Polyhedral Playground </h2></a>
+            <a href="https://kriepiekrollie.github.io/polyhedra/"><h2> Polyhedral Playground </h2></a>
             <p> I wanted to create a fun and pretty way to play with polyhedra so this is what I came up with. </p>
             <p> I'm still adding shapes and I hope to have a huge catalog and more cool features by the end. </p>
           </div>
@@ -17,7 +18,7 @@ export default function Projects() {
         <div className="projectItem">
           <img src="setgame.jpg" />
           <div className="projectText">
-            <a href="kriepiekrollie.github.io/setgame"><h2> Set Game </h2></a>
+            <a href="https://kriepiekrollie.github.io/set-game/"><h2> Set Game </h2></a>
             <p> A recreation of a card game I like very much </p>
             <p> This project is kinda abandoned but it works fine and was more of a way for me to practice webdev.</p>
           </div>
